c83a35333ff1c6544a6d7a82b48ff7da637e6985
Park, Jonghyeon <belline0124@gmail.com>
2024-10-04 02:10:36 +0900

    add camera module

---
+++ arduino/camera/ov7670.h
@@ -0,0 +1,14 @@
+#define CAM_PIN_SDA   A4
+#define CAM_PIN_SCL   A5
+#define CAM_PIN_D0    2
+#define CAM_PIN_D1    3
+#define CAM_PIN_D2    4
+#define CAM_PIN_D3    5
+#define CAM_PIN_D4    6
+#define CAM_PIN_D5    7
+#define CAM_PIN_D6    8
+#define CAM_PIN_D7    9
+#define CAM_PIN_PCLK 10
+#define CAM_PIN_VSNC 11
+#define CAM_PIN_HREF 12
+#define CAM_PIN_XCLK NULL
